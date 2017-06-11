bc3ef2cc7e11cd45634dd93db92d1dbc4e4c3cb4
Jim Lesperance <lesperancejim.lj@hotmail.com>
2017-06-11 22:28:24 +0200

    remove pageviews

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import VelocityBarChart from './VelocityBarChart.jsx';
 import BurnDownLineChart from './BurnDownLineChart.jsx';
 import VelocityPieChart from './VelocityPieChart.jsx';
-import PageViews from './PageViews.jsx';
+
 export default {
 	VelocityBarChart,
 	BurnDownLineChart,
-	VelocityPieChart,
-	PageViews
-
+	VelocityPieChart
 };
